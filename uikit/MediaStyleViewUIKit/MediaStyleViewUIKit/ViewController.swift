3a2e560218b5dbfebb7aaaa906887bcbbfec077d
Fumiya Sakai <just1factory@gmail.com>
2020-12-09 12:02:22 +0900

    UIKitとの実装を比べるためにUIKitのプロジェクトを追加

---
+++ uikit/MediaStyleViewUIKit/MediaStyleViewUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaStyleViewUIKit
+//
+//  Created by 酒井文也 on 2020/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
