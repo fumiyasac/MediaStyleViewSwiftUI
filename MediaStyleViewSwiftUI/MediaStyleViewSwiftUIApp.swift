a244e0e349537c8c29a390ec4b4a3d197724db28
Fumiya Sakai <just1factory@gmail.com>
2021-10-01 20:22:13 +0900

    Replace Xcode13

---
+++ MediaStyleViewSwiftUI/MediaStyleViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaStyleViewSwiftUIApp.swift
+//  MediaStyleViewSwiftUI
+//
+//  Created by 酒井文也 on 2021/10/01.
+//
+
+import SwiftUI
+
+@main
+struct MediaStyleViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
