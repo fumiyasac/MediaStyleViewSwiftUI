dd607895a66e70b98b83c1f8ca3940c64ea25a4a
fumiyasac <just1factory@gmail.com>
2025-04-21 07:59:59 +0900

    改めてiOSプロジェクトを整備する

--- MediaStyleViewSwiftUI/Package.swift
+++ MediaStyleViewSwiftUI/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ import PackageDescription
 
 let package = Package(
     name: "MediaStyleViewSwiftUI",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(name: "AppFeature", targets: ["AppFeature"])
     ],
